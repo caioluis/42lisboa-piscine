23d244166a994b6358143fe7a05b69d4b66e1e38
caioluis <ocaioluis@gmail.com>
2021-08-18 14:25:43 +0100

    Piscine C03 - Exercises

--- C Piscine/C03/ex05/ft_strlcat.c
+++ C Piscine/C03/ex05/ft_strlcat.c
@@ -25,15 +25,18 @@ unsigned int	ft_strlen(char *str)
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	initialLength;
+	unsigned int	j;
 
-	initialLength = ft_strlen(dest);
-	i = 0;
-	while (src[i] != '\0' && i < size - ft_strlen(dest) - 1)
+	if (size <= ft_strlen(dest))
+		return (size + ft_strlen(src));
+	i = ft_strlen(dest);
+	j = 0;
+	while (src[j] != '\0' && i + 1 < size)
 	{
-		dest[initialLength + i] = src[i];
+		dest[i] = src[j];
 		i++;
+		j++;
 	}
-	dest[ft_strlen(dest)] = '\0';
-	return (initialLength + ft_strlen(src));
+	dest[i] = '\0';
+	return (ft_strlen(dest) + ft_strlen(&src[j]));
 }
